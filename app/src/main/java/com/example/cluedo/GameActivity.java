4cbc1c14f119c5e051a838d21cd86159f6ae7f21
Stepan Kuznetsov <stepan14511@gmail.com>
2019-05-01 00:44:05 +0300

    Added sending names_list to the game activity

--- app/src/main/java/com/example/cluedo/GameActivity.java
+++ app/src/main/java/com/example/cluedo/GameActivity.java
@@ -2,12 +2,35 @@ package com.example.cluedo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class GameActivity extends AppCompatActivity {
+
+    public static String NAME_STEPAN = "STEPAN";
+    public static String NAME_ANYA = "ANYA";
+    public static String NAME_MAMA = "MAMA";
+    public static String NAME_PAPA = "PAPA";
+
+    private ArrayList<String> names_list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        if((getIntent() != null ) && (getIntent().hasExtra("names_list")))
+            names_list = getIntent().getStringArrayListExtra("names_list");
+
+        TextView textView = findViewById(R.id.name_1);
+        textView.setText(names_list.get(0));
+        textView = findViewById(R.id.name_2);
+        textView.setText(names_list.get(1));
+        textView = findViewById(R.id.name_3);
+        textView.setText(names_list.get(2));
+        textView = findViewById(R.id.name_4);
+        textView.setText(names_list.get(3));
     }
 }
--- app/src/main/java/com/example/cluedo/GameInitActivity.java
+++ app/src/main/java/com/example/cluedo/GameInitActivity.java
@@ -1,13 +1,72 @@
 package com.example.cluedo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class GameInitActivity extends AppCompatActivity {
+
+    private static int NUMBER_OF_PLAYERS = 4;
+    private ArrayList<String> names_list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_init);
+
+        initButtons();
+    }
+
+    private void initButtons(){
+        Button button = findViewById(R.id.btn_anya);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                press_btn(GameActivity.NAME_ANYA);
+                Button button = findViewById(R.id.btn_anya);
+                button.setVisibility(View.INVISIBLE);
+            }
+        });
+        button = findViewById(R.id.btn_mama);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                press_btn(GameActivity.NAME_MAMA);
+                Button button = findViewById(R.id.btn_mama);
+                button.setVisibility(View.INVISIBLE);
+            }
+        });
+        button = findViewById(R.id.btn_papa);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                press_btn(GameActivity.NAME_PAPA);
+                Button button = findViewById(R.id.btn_papa);
+                button.setVisibility(View.INVISIBLE);
+            }
+        });
+        button = findViewById(R.id.btn_stepan);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                press_btn(GameActivity.NAME_STEPAN);
+                Button button = findViewById(R.id.btn_stepan);
+                button.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private void press_btn(String name){
+        names_list.add(name);
+        if(names_list.size() >= NUMBER_OF_PLAYERS){
+            Intent intent = new Intent(GameInitActivity.this, GameActivity.class);
+            intent.putExtra("names_list", names_list);
+            startActivity(intent);
+        }
     }
 }
